bd051df7ab145cb6eaf84d3e1ae1c07a830cebc3
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-04-20 17:36:23 +0200

    adding scripts

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install -y ruby-full
+
+echo 'export GEM_HOME=$HOME/gems'       >> ~/.bashrc
+echo 'export PATH=$HOME/gems/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+gem install rubygems-update jekyll bundler
+
+
+
